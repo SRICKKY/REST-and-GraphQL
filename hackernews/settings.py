f0ef92d63e38fbad27b4a5013bf2402834c35273
SRICKKY <kumar_srikant@outlook.com>
2018-10-13 01:23:07 +0530

    Implemented Query N Mutation

--- hackernews/settings.py
+++ hackernews/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'graphene_django',
+    'links',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+GRAPHENE = {
+    'SCHEMA': 'hackernews.schema.schema'
+}
